a1128cc21e8a4179802f2578025496ebb65b7996
MarindanyTracy <marindanytracy0@gmail.com>
2023-01-08 18:42:00 +0300

    Navigation

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,66 @@
+import {Navbar,Container} from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Logo from '../../assets/img/logo.svg';
+import navIcon1 from '../../assets/img/nav-icon1.svg';
+import navIcon2 from '../../assets/img/nav-icon2.svg';
+import navIcon3 from '../../assets/img/nav-icon3.svg';
+
+
+export const NavBar = () => {
+
+const [activeLink, setActiveLink] = useState('home');
+const [scrolled, setScrolled] = useState(false);
+
+useEffect(() => {
+  const onScroll = () => {
+    if(window.scrollY > 50) {
+      setScrolled(true)
+    } else {
+      setScrolled(false);
+    }
+  }
+
+  window.addEventListener('scroll', onScroll);
+
+  return () => window.removeEventListener('scroll', onScroll);
+
+}, [])
+
+const onUpdateActiveLink = (value) => {
+  setActiveLink(value);
+}
+
+  return (
+    <Navbar className={scrolled ? 'scrolled' : ''} expand="lg" >
+      <Container>
+        <Navbar.Brand href="#home">
+          <img src={Logo} alt='Logo' />
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav"> 
+        <span className='navbar-toggler-icon'></span>
+        </Navbar.Toggle>
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" onClick={() => onUpdateActiveLink('home')} className={activeLink === 'home' ? 'active navbar-link': 'navbar-link'}>Home</Nav.Link>
+            <Nav.Link href="#skills" onClick={() => onUpdateActiveLink('skills')} className={activeLink === 'skills' ? 'active navbar-link': 'navbar-link'}>Skills</Nav.Link>
+            <Nav.Link href="#projects" onClick={() => onUpdateActiveLink('projects')} className={activeLink === 'projects' ? 'active navbar-link': 'navbar-link'}>Projects</Nav.Link>
+          </Nav>
+
+          <span className='navbar-text'>
+            <div className='social-icon'>
+              <a href="https://www.linkedin.com/"><img src={navIcon1} alt="" /></a>
+              <a href="https://www.facebook.com/"><img src={navIcon2} alt="" /></a>
+              <a href="https://www.instagram.com/"><img src={navIcon3} alt="" /></a>
+            </div>
+            <button className='vvd'onClick={() => console.log('connect')}>
+              <span>Let's Connect</span>
+            </button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+};
