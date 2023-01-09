cee577044f2795318ebc66bfc54bb5d1b4286da5
MarindanyTracy <marindanytracy0@gmail.com>
2023-01-09 13:20:33 +0300

    The Main Banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { NavBar } from './components/Navbar/Navbar';
+import  Banner from './components/Banner/Banner';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
     <div className="App">
      <NavBar />
+     <Banner />
     </div>
   );
 }
