8eac9e4967de619acfacd1923c29fa49469a91f0
MarindanyTracy <marindanytracy0@gmail.com>
2023-01-10 21:57:58 +0300

    Skills

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { NavBar } from './components/Navbar/Navbar';
 import  Banner from './components/Banner/Banner';
+import Skills from './components/Skills/Skills';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
      <NavBar />
      <Banner />
+     <Skills />
     </div>
   );
 }
